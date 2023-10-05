08695a0714971c29d229140e943ff90224c305d1
Peter Park <peterjpark@pm.me>
2023-10-05 15:01:33 -0400

    Restructure client side routing

---
+++ src/data.ts
@@ -0,0 +1,24 @@
+export const users = [
+  {
+    id: "abc123zxc", // PK
+    handle: "qkrwns",
+    premium: false,
+    followers: [],
+    bookmarks: [],
+  },
+  {
+    id: "456qwe420",
+    handle: "honomo",
+    premium: true,
+    followers: [],
+    bookmarks: [],
+  },
+];
+
+export const yaps = [
+  {
+    id: "69four20",
+    user_id: "abc123zxc",
+    likes: "",
+  },
+];
